cdf6351135f712fc87912de503c1f1bda72288f7
fsarrias <fsarrias@kolab.es>
2020-05-18 18:29:13 +0200

    Subo la parte 3 del ejercicio

--- src/main/java/com/gettingstarted/helloworld/HelloController.java
+++ src/main/java/com/gettingstarted/helloworld/HelloController.java
@@ -1,13 +1,15 @@
 package com.gettingstarted.helloworld;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
         @RequestMapping("/")
-        public String index(){
-            return "HOLA MUNDO";
+        public String index(@RequestParam(name = "name", defaultValue = "WORLD") String name){
+            return "HOLA " + name;
         }
 
 }
