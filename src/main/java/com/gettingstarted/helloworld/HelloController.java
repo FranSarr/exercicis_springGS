bd8a385e5ded238bec7ea36fe15628a5e4d80d58
fsarrias <fsarrias@kolab.es>
2020-05-19 18:25:29 +0200

    le quito  @CrossOrigin(origins = "http://localhost")

--- src/main/java/com/gettingstarted/helloworld/HelloController.java
+++ src/main/java/com/gettingstarted/helloworld/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-        @CrossOrigin(origins = "http://localhost")
         @RequestMapping("/")
         public String index(@RequestParam(name = "name", defaultValue = "WORLD") String name){
             return "HOLA " + name;
