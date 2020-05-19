79d3243312cdb7cf2d03638df59b3e78ba16f1dc
fsarrias <fsarrias@kolab.es>
2020-05-19 10:36:54 +0200

    Parte 4, me da un error de CORS en la consola del Chromium Access to XMLHttpRequest at 'http://localhost:8080/' from origin 'http://localhost:63342' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- src/main/java/com/gettingstarted/helloworld/HelloController.java
+++ src/main/java/com/gettingstarted/helloworld/HelloController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class HelloController {
         @RequestMapping("/")
         public String index(@RequestParam(name = "name", defaultValue = "WORLD") String name){
             return "HOLA " + name;
         }
-
 }
+
+
