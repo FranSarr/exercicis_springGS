11ec566def5ac2fb2dd4fbb8029504591c8499f7
fsarrias <fsarrias@kolab.es>
2020-05-19 17:11:25 +0200

    Sigue sin funcionar pese a que le he añadido headers y @CrossOrigin(origins = "http://localhost")

--- src/main/java/com/gettingstarted/helloworld/HelloController.java
+++ src/main/java/com/gettingstarted/helloworld/HelloController.java
@@ -1,14 +1,11 @@
 package com.gettingstarted.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class HelloController {
+        @CrossOrigin(origins = "http://localhost")
         @RequestMapping("/")
         public String index(@RequestParam(name = "name", defaultValue = "WORLD") String name){
             return "HOLA " + name;
